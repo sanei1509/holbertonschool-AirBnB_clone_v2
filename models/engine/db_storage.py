dee4ac19d33ab9232a87c966c7312912bdc3c1de
Santiago Neira <santiagoneira52@gmail.com>
2022-08-08 23:48:58 +0000

    changes on db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -24,8 +24,31 @@ class DBStorage():
 	def __init__(self):
 		if __name__ == "__main__":
 			self.__engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(os.getenv("HBNB_MYSQL_USER"), os.getenv("HBNB_MYSQL_PWD"), os.getenv("HBNB_MYSQL_DB")), pool_pre_ping=True)
-    		Base.metadata.create_all(engine)
+    		Base.metadata.create_all(self.__engine)
+			val_env = os.getenv("HBNB_ENV")
+			if val_env == "test":
+				DBStorage.__table__.drop(self.__engine)
 
-		all(self, cls=None):
+
+	
+	def	all(self, cls=None):
 			"""peticion de todos los objetos"""
-        	return self.__objects
+			clases = {
+               'User': User, 'Place': Place,
+               'State': State, 'City': City, 'Amenity': Amenity,
+               'Review': Review
+			}
+			name_db = os.getenv("HBNB_MYSQL_DB")
+			obj_dic = {}
+			if not cls:
+				all_data = self.__session.query(cls)
+				for obj in all_data:
+					key = cls.__name__ + "." + obj.id
+					obj_dic[key] = obj
+				return obj_dic
+			else:
+				data = self.__session.query(cls)
+				for obj in data:
+					key = cls.__name__ + "." + obj.id
+					obj_dic[key] = obj
+        	return obj_dic
