d442d37911a1c216b6245da3d8348682210a1c2b
Santiago Neira <santiagoneira52@gmail.com>
2022-08-09 19:00:23 +0000

    version, try task 10

--- models/place.py
+++ models/place.py
@@ -7,38 +7,51 @@ from models.review import Review
 import os
 
 
+if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    place_amenity = Table("place_amenity", Base.metadata,
+                          Column("place_id", String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True))
+
+
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
 
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-    amenity_ids = []
-
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms = Column(Integer, nullable=False, default=0)
+        max_guest = Column(Integer, nullable=False, default=0)
+        price_by_night = Column(Integer, nullable=False, default=0)
+        latitude = Column(Float, nullable=True)
+        longitude = Column(Float, nullable=True)
+        amenity_ids = []
+
         reviews = relationship('Review', backref='place',
                                cascade='all, delete-orphan')
-
-        place_amenity = Table("place_amenity", Base.metadata,
-                              Column("place_id", String(60),
-                                     ForeignKey("places.id"),
-                                     primary_key=True),
-                              Column("amenity_id", String(60),
-                                     ForeignKey("amenities.id"),
-                                     primary_key=True))
-
         r_amenities = relationship("Amenity",  secondary="place_amenity",
                                    viewonly=False)
-
     else:
+        city_id = ""
+        user_id = ""
+        name = ""
+        description = ""
+        number_rooms = 0
+        number_bathrooms = 0
+        max_guest = 0
+        price_by_night = 0
+        latitude = 0.0
+        longitude = 0.0
+        amenity_ids = []
+
         @property
         def reviews(self):
             list_reviews = []
