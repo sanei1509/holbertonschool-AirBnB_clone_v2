701644e069ccbc62052be56abad4153b13f87dd1
Santiago Neira <santiagoneira52@gmail.com>
2022-08-09 07:08:19 +0000

    create city class

--- models/city.py
+++ models/city.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+# from sqlalchemy.orm import relationship
 from models.place import Place
 
 
@@ -11,6 +11,6 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
 
-    # state_id = Column(String(60), nullable=False,)
-    # name = Column(String(128), ForeignKey("states.id"), nullable=False)
+    state_id = Column(String(60), nullable=False,)
+    name = Column(String(128), ForeignKey("states.id"), nullable=False)
     # places = relationship("Place", cascade="all,delete", backref='cities')
