2c72412640d6bb707e5369c8f4e7d7a5abfebf81
Santiago Neira <santiagoneira52@gmail.com>
2022-08-09 03:26:03 +0000

    try fix task 6 , no workinng

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,9 +7,9 @@ Class DBStorage:
 import json
 import os
 
-from sqlalchemy.orm import scoped_session, scoped_session
-from sqlalchemy import create_engine
 from models.base_model import Base
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import create_engine
 from models.user import User
 from models.city import City
 from models.review import Review
@@ -24,34 +24,36 @@ class DBStorage():
     __session = None
 
     def __init__(self):
-        if __name__ == "__main__":
-            self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
-                                          os.getenv("HBNB_MYSQL_USER"),
-                                          os.getenv("HBNB_MYSQL_PWD"),
-                                          os.getenv("HBNB_MYSQL_HOST"),
-                                          os.getenv("HBNB_MYSQL_DB")),
-                                          pool_pre_ping=True)
-            Base.metadata.create_all(self.__engine)
-            val_env = os.getenv("HBNB_ENV")
-            if val_env == "test":
-                Base.metadata.drop_all(self.__engine)
+        """Inicializamos """
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(
+                                      os.getenv("HBNB_MYSQL_USER"),
+                                      os.getenv("HBNB_MYSQL_PWD"),
+                                      os.getenv("HBNB_MYSQL_HOST"),
+                                      os.getenv("HBNB_MYSQL_DB")),
+                                      pool_pre_ping=True)
+        Base.metadata.create_all(self.__engine)
+        val_env = os.getenv("HBNB_ENV")
+        if val_env == "test":
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """peticion de todos los objetos"""
+        from models.base_model import BaseModel
+        """ all """
+        clases = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+        }
+        cls_list = []
+        if cls is None:
+            cls_list.append(clases[cls])
         obj_dic = {}
-        if not cls:
-            clases = ["User", "State", "City", "Amenity", "Place", "Review"]
-            for x in clases:
-                all_data = self.__session.query(x)
-                key = cls.__name__ + "." + x.id
-                obj_dic[key] = x
-            return obj_dic
-        else:
-            data = self.__session.query(cls)
-            for obj in data:
-                key = cls.__name__ + "." + obj.id
+        for clase in cls_list:
+            all_data = self.__session.query(clase).all()
+            for obj in all_data:
+                key = obj.__class__.__name__ + "." + obj.id
                 obj_dic[key] = obj
-                return obj_dic
+        return obj_dic
 
     def new(self, obj):
         '''add the boj to current db session'''
@@ -67,9 +69,8 @@ class DBStorage():
             self.__session.delete(obj)
 
     def reload(self):
-        '''creat all tables and create current db session'''
-        from models.base_model import Base
+        """Creates all the tables in the db and starts the session"""
         Base.metadata.create_all(self.__engine)
-        session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session)
+        ses = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(ses)
         self.__session = Session()
