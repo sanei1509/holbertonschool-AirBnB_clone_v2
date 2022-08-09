936c858e4b98da32a8e474de3bafb4231a91b040
MateoArbini <mateoarbini1998@gmail.com>
2022-08-09 09:50:32 -0700

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -33,7 +33,8 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.to_dict())
+        cls = type(self).__name__
+        return '[{}] ({}) {}'.format(cls, self.id, self.to_dict())
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
