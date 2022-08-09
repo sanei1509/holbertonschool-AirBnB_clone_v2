6b0dc619a3d47b4eee2d779792c7c7ab368c7b2f
MateoArbini <mateoarbini1998@gmail.com>
2022-08-08 17:13:56 -0700

    modified init to change storage type

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
 
+import os
 
-storage = FileStorage()
-storage.reload()
+db_value = os.getenv("HBNB_TYPE_STORAGE")
+if db_value == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
