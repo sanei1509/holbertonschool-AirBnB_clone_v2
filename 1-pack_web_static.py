98ab15cfb477807ad54ecc82433481d13b9eebc6
Santiago Neira <santiagoneira52@gmail.com>
2022-08-24 21:02:31 +0000

    create tar file with fabric

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+script para generar un archivo .'tgz'
+-	desde el contenido de web static del AirBnB Clone repo.
+-	using the function do_pack
+"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """
+    function que devuelve el archivo
+    sino devuelve None
+    """
+    local("mkdir -p versions/")
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    name = "versions/web-static_" + time
+    try:
+        local("tar -cvzf " + name + ".tgz web_static")
+        return f"{name}.tgz"
+    except Exception:
+        return None
