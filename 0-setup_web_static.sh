b79a00df9ce71bcaa2d13ebb6dc004fe9f1f98bf
Santiago Neira <santiagoneira52@gmail.com>
2022-08-25 15:34:38 +0000

    give permissions

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,15 +3,13 @@
 # -f del ln -> si existe debe eliminarse y volver a crearse
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-echo "
-<html>
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>
-" > /data/web_static/releases/test/index.html
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current 
 chown -R ubuntu:ubuntu /data/
 sed -i "55i location /hbnb_static/ {\nalias /data/web_static/current/;\n}\n" /etc/nginx/sites-enabled/default
