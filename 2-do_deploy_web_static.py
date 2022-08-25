8e9d109ba7f59c9770418aca08b8b80a886a5893
Santiago Neira <santiagoneira52@gmail.com>
2022-08-25 16:46:52 +0000

    fix task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,19 +18,21 @@ def do_deploy(archive_path):
     else:
         try:
             put(archive_path, "/tmp/")
-            """ nombre del archivo con .tgz """
-            file_name = archive_path.split("/")[1]
-            """ nombre del archivo sin .tgz """
-            file_name2 = file_name.split(".")[0]
-            """ ruta completa hacia el archivo """
-            final_name = "/data/web_static/releases/" + file_name2 + "/"
-            run("mkdir -p " + final_name)
-            run("tar -xzf /tmp/" + file_name + " -C " + final_name)
-            run("rm /tmp/" + file_name)
-            run("mv " + final_name + "web_static/* " + final_name)
-            run("rm -rf " + final_name + "web_static")
+            # Nombre del archivo CON la extension (.tgz)
+            filename = archive_path.split("/")[1]
+            # Nombre del archivo SIN la extension
+            filename_no_ext = filename.split(".")[0]
+            # Ruta completa hacia el archivo SIN extension
+            path = "/data/web_static/releases/" + filename_no_ext + "/"
+
+            # Create directory sino existe aun
+            run("mkdir -p " + path)
+            run("tar -xzf /tmp/" + filename + " -C " + path)
+            run("mv " + path + "web_static/*" + " " + path)
+            run("rm /tmp/{}".format(filename))
+            run("rm -rf " + path + "web_static")
             run("rm -rf /data/web_static/current")
-            run("ln -s " + final_name + " /data/web_static/current")
+            run("ln -s " + path + " /data/web_static/current")
             print("New version deployed!")
             return True
         except Exception:
