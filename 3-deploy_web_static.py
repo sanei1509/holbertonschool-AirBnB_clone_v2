6846985ce6a2a78272776a202ce1bd005916f58f
Santiago Neira <santiagoneira52@gmail.com>
2022-08-25 17:08:39 +0000

    deploy function

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,20 +8,36 @@ import os
 from fabric.api import run, put, env
 
 env.hosts = ['54.226.169.216', '54.91.221.19']
-# env.user = "ubuntu"
+env.user = "ubuntu"
+
+
+def do_pack():
+    """
+    function que devuelve el archivo
+    sino devuelve None
+    """
+    local("mkdir -p versions/")
+    time = datetime.now().strftime("%Y%M%d%H%M%S")
+    name_file = "versions/web_static_{}.tgz".format(time)
+    try:
+        local("tar -cvzf {} web_static/".format(name_file))
+        return name_file
+    except Exception:
+        return None
+
 
 def do_deploy(archive_path):
     """Fabric script distributes archive to web servers """
-    if os.path.exists(archive_path) == False:
+    if os.path.exists(archive_path) is False:
         return False
     else:
         try:
             put(archive_path, "/tmp/")
-            """ file_name name of file with .tgz """
+            """ nombre del archivo con .tgz """
             file_name = archive_path.split("/")[1]
-            """ file_name2 name of file without .tgz """
+            """ nombre del archivo sin .tgz """
             file_name2 = file_name.split(".")[0]
-            """ final_name name of path of directory """
+            """ ruta completa hacia el archivo """
             final_name = "/data/web_static/releases/" + file_name2 + "/"
             run("mkdir -p " + final_name)
             run("tar -xzf /tmp/" + file_name + " -C " + final_name)
@@ -32,5 +48,14 @@ def do_deploy(archive_path):
             run("ln -s " + final_name + " /data/web_static/current")
             print("New version deployed!")
             return True
-        except:
+        except Exception:
             return False
+
+
+def deploy():
+    """funcion para ejecutar la función completa"""
+    path = do_pack()
+    if os.path.exists(path):
+        return do_deploy(path)
+    else:
+        return False
