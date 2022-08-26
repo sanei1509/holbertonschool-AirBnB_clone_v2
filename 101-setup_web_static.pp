a46aca5dc278ef6765b8e5c0033fbad822cb66d8
Santiago Neira <santiagoneira52@gmail.com>
2022-08-26 04:13:36 +0000

    try automatize the task 0

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -6,17 +6,20 @@ exec { 'update':
   before   => Exec['nginx'],
 }
 
+#Instalar nginx
 exec {'nginx':
   provider => shell,
   command  => 'sudo apt-get -y install nginx',
   before   => Exec['inicio web server'],
 }
 
+#Crear carpetas
 exec {'inicio web server':
   provider => shell,
   command  => 'sudo service nginx start',
   before   => Exec['create_directory'],
 }
+
 
 exec {'create_directory':
   provider => shell,
@@ -24,12 +27,14 @@ exec {'create_directory':
   before   => Exec['create_other_directory'],
 }
 
+#Creando link simbolico
 exec {'create_other_directory':
   provider => shell,
   command  => 'sudo mkdir -p /data/web_static/shared/',
   before   => Exec['content_for_html'],
 }
 
+#Añadiendo contenido estatico
 exec {'content_for_html':
   provider => shell,
   command  => 'echo "
@@ -41,21 +46,24 @@ exec {'content_for_html':
   </body>
 </html>" > /data/web_static/releases/test/index.html
 ',
-  before   => Exec['create_symbolic_link'],
+  before   => Exec['creo symbolic link'],
 }
 
-exec {'create_symbolic_link':
+#Creando link simbolico
+exec {'creo symbolic link':
   provider => shell,
   command  => 'sudo ln -sf /data/web_static/releases/test/ /data/web_static/current',
-  before   => Exec['put_fabric'],
+  before   => Exec['create alias config'],
 }
 
-exec {'put_fabric':
+#Creando la configuración del alias para nuestra vista
+exec {'create alias config':
   provider => shell,
   command  => 'sed -i "55i location /hbnb_static/ {\nalias /data/web_static/current/;\n}\n" /etc/nginx/sites-enabled/default',
   before   => Exec['restart web server'],
 }
 
+#Reiniciamos nginx para confirmar configuración
 exec {'restart web server':
   provider => shell,
   command  => 'sudo service nginx restart',
