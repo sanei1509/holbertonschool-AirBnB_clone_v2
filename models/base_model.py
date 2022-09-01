dea83a77ca089d3abe4cff1c4a9bf7b6f9530fc1
Santiago Neira <santiagoneira52@gmail.com>
2022-09-01 20:32:59 +0000

    change on the init of base model

--- models/base_model.py
+++ models/base_model.py
@@ -17,19 +17,23 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            # storage.new(self)
-            # lo movemos para el save antes de llamarlo
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                if key == "updated_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == "created_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                elif key != "__class__":
+                    setattr(self, key, value)
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
 
     def __str__(self):
         """Returns a string representation of the instance"""
