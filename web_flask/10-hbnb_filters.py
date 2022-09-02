8c720100e054d31bc767be59742a1288fe5fe021
Santiago Neira <santiagoneira52@gmail.com>
2022-09-02 16:04:10 +0000

    fix problem of task 11

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -2,7 +2,6 @@
 from flask import Flask, render_template, request
 from models import storage
 from models.state import State
-from models import storage
 from models.amenity import Amenity
 from models.place import Place
 """
