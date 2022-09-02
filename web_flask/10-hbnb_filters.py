ba9a7e154f9bf43ccc8cfaaa7251aafbc2988cf1
Santiago Neira <santiagoneira52@gmail.com>
2022-09-02 18:12:52 +0000

    fix problem of connection

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
-"""create app"""
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 from models import storage
 from models.state import State
 from models.amenity import Amenity
 from models.place import Place
+"""
+Starts a small web aplication
+- listening on 0.0.0.0 port 5000
+- display "Hello HBNB!"
+
+Task 10
+- renderizar un contenido html completo del clon
+"""
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def tear_down(self):
-    """
-    After each request you must remove the current SQLAlchemy Session
-    """
+def off_all(self):
+    """Cierro la sesión de my sql"""
     storage.close()
 
 
-@app.route("/hbnb_filters", strict_slashes=False)
-def states_list():
-    """Import data from storage"""
+@app.route('/hbnb_filters', strict_slashes=False)
+def filtro_data():
+    """Contenido a mostra en la página html"""
     states = storage.all(State).values()
-    amenities = storage.all(Amenity).values()
-    print(amenities)
-    return render_template("10-hbnb_filters.html", states=states, amenities=amenities)
+    lista_ams = storage.all(Amenity).values()
+    return render_template("10-hbnb_filters.html", states=states,
+                           amenities=lista_ams)
 
 
-if __name__ == '__main__':
-    """app run"""
+"""la app no corre al ser importada"""
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
